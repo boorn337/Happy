e894e609aaeeecce61d620f2de788dd782b7b79d
boorn337 <128753056+boorn337@users.noreply.github.com>
2023-03-23 23:59:39 +0200

    Hello

---
+++ Софійка/test.js
@@ -0,0 +1,5 @@
+// Set the height of all grid items to the height of the tallest one
+function setGridItemHeight() {
+  let gridItems = document.querySelectorAll(".grid-item");
+  let maxHeight = 0;
+}
