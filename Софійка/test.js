d2caf8529f85d2c7d47de3a70f27c121a54872c0
boorn337 <128753056+boorn337@users.noreply.github.com>
2023-03-24 00:08:53 +0200

    Delete Софійка directory

--- Софійка/test.js
+++
@@ -1,5 +0,0 @@
-// Set the height of all grid items to the height of the tallest one
-function setGridItemHeight() {
-  let gridItems = document.querySelectorAll(".grid-item");
-  let maxHeight = 0;
-}
